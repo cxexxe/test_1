5327a8d83fae5d95f66ddf8c6da6a9287de47a7f
mingyeong <dld091016@gmail.com>
2024-11-20 12:22:13 +0900

    test6

--- js/mobile.js
+++ js/mobile.js
@@ -1,15 +1,4 @@
-let startY = 0;
 
-document.addEventListener('touchstart', function (event) {
-    startY = event.touches[0].clientY;
-}, { passive: false });
-
-document.addEventListener('touchmove', function (event) {
-    const currentY = event.touches[0].clientY;
-    if (window.scrollY === 0 && currentY > startY) {
-        event.preventDefault();
-    }
-}, { passive: false });
 
 function initializeMobile() {
     console.log('Mobile JS loaded');
@@ -42,48 +31,48 @@ function initializeMobile() {
     // 카테고리별 프로젝트 데이터 분리
     const projectsData = {
         digital: [
-            { title: 'Ready to play the GAME?', author: '김민경', image: 'images/works/김민경A.png', link: 'https://dibi00.github.io/2301110137_kimmingyeong_zolzak/' },
-            { title: '쌩쌩몰 웹사이트 리디자인', author: '김민경', image: 'images/works/김민경B.png', link: 'https://www.figma.com/proto/7A258HOdGLDcEQ0ElQo095/%EC%8C%A9%EC%8C%A9%EB%AA%B0?page-id=275%3A2496&node-id=275-2497&node-type=frame&viewport=792%2C679%2C0.09&t=b7Z8ttguyi1IH6GR-1&scaling=scale-down&content-scaling=fixed' },
-            { title: '폴리의 아이', author: '김수빈', image: 'images/works/김수빈.jpg', link: 'https://drive.google.com/file/d/13naI6wg_H3sXl6C4aWDBW9sUWvtsdc2Q/view?usp=sharing' },
-            { title: 'WELAAA 2.0 프로모션 웹', author: '김수아', image: 'images/works/김수아.jpg', link: 'https://suakim1021.github.io/2024-graduate-portfolio/web_welaaa.html' },
-            { title: 'Portfolio', author: '김승찬', image: 'images/works/김승찬.jpg', link: 'https://yellowgrow.github.io/webportfolio/' },
-            { title: 'Portfolio', author: '김준희', image: 'images/works/김준희.png', link: 'https://mangoismycat.github.io/port/' },
-            { title: 'Portfolio', author: '김태호', image: 'images/works/김태호.png', link: 'https://kimtaehh.github.io/kkth1234/' },
-            { title: 'RYU Summary', author: '류시은', image: 'images/works/류시은.png', link: 'https://ryu-sieun.github.io/2301110153_RYU/' },
-            { title: 'I WILL MAKE MY DESIGN PATH', author: '박가람', image: 'images/works/박가람.png', link: 'https://1220asc.github.io/test1113_c/' },
-            { title: 'Communicative Designer', author: '심재정', image: 'images/works/심재정.png', link: 'https://simpsonii.github.io/shimjaejeong-Portfolio-Sites/' },
-            { title: 'A NEW Encounter with Ohui', author: '양성우', image: 'images/works/양성우.jpg', link: 'https://yangseongwoo.github.io/ohuiwebsite/' },
-            { title: '그린피스 웹사이트 리디자인', author: '우수지', image: 'images/works/우수지.jpg', link: 'https://wsj6328.github.io/green/' },
-            { title: '그리드와 소금', author: '원세연', image: 'images/works/원세연.png', link: 'https://seyeonwon.github.io/web__portfolio/' },
-            { title: 'What’s on my desk', author: '유지수', image: 'images/works/유지수.jpg', link: 'https://wltn510.github.io/1107/' },
-            { title: '서울 자전거 따릉이', author: '윤 비', image: 'images/works/윤비.png', link: 'https://drive.google.com/file/d/1lYo49TlrjjIXF-Ev-oR05J9aIZa0Ygw8/view?usp=sharing' },
-            { title: '카카오뱅크 웹사이트 리디자인', author: '이세윤', image: 'images/works/이세윤1.jpg', link: 'https://leeeseyoon.github.io/kakaobank_2/' },
-            { title: '위홈 앱 리디자인', author: '이세윤', image: 'images/works/이세윤2.jpg', link: 'https://drive.google.com/file/d/13hy-fX0RTd7LBDh5lOAvT34jYPpwB-FR/view?usp=sharing' },
-            { title: 'Portfolio', author: '이지우', image: 'images/works/이지우.jpg', link: 'https://jiwooyoooooo.github.io/portfolio/#works' },
-            { title: '광염 소나타 책 표지', author: '임 율', image: 'images/works/임율.jpg', link: 'https://drive.google.com/file/d/10WNQMcpWPJCUxRxZ1LAAWGT8Wj2dJjef/view?usp=sharing' },
-            { title: '유튜브 앱 리디자인', author: '임현준', image: 'images/works/임현준.png', link: 'https://drive.google.com/file/d/1GzokKcUFusyRB9sxcCd2erSKArgJD_2h/view?usp=sharing' },
-            { title: 'Portfolio', author: '장천수', image: 'images/works/장천수.png', link: 'https://1000soo.github.io/cs_work/' },
-            { title: 'Portfolio', author: '최은지', image: 'images/works/최은지.jpg', link: 'https://choieunjiji.github.io/port/' },
-            { title: '프라임 비디오 앱', author: '최훈석', image: 'images/works/최훈석.png', link: 'https://drive.google.com/file/d/1d0Hjt6WTWM_oZm4IKAXztCFNDly1bO60/view?usp=sharing' },
-            { title: '크린나라 앱 리디자인', author: '황세희', image: 'images/works/황세희.png', link: 'https://drive.google.com/file/d/1j2KgnJsLtmfSR2nEsCLlLPtj3R-u2nWt/view?usp=sharing' }
+            { id: 1, title: 'Ready to play the GAME?', author: '김민경', image: 'images/works/김민경A.png', link: 'https://dibi00.github.io/2301110137_kimmingyeong_zolzak/' },
+            { id: 2, title: '쌩쌩몰 웹사이트 리디자인', author: '김민경', image: 'images/works/김민경B.png', link: 'https://www.figma.com/proto/7A258HOdGLDcEQ0ElQo095/%EC%8C%A9%EC%8C%A9%EB%AA%B0?page-id=275%3A2496&node-id=275-2497&node-type=frame&viewport=792%2C679%2C0.09&t=b7Z8ttguyi1IH6GR-1&scaling=scale-down&content-scaling=fixed' },
+            { id: 3, title: '폴리의 아이', author: '김수빈', image: 'images/works/김수빈.jpg', link: 'https://drive.google.com/file/d/13naI6wg_H3sXl6C4aWDBW9sUWvtsdc2Q/view?usp=sharing' },
+            { id: 4, title: 'WELAAA 2.0 프로모션 웹', author: '김수아', image: 'images/works/김수아.jpg', link: 'https://suakim1021.github.io/2024-graduate-portfolio/web_welaaa.html' },
+            { id: 5, title: 'Portfolio', author: '김승찬', image: 'images/works/김승찬.jpg', link: 'https://yellowgrow.github.io/webportfolio/' },
+            { id: 6, title: 'Portfolio', author: '김준희', image: 'images/works/김준희.png', link: 'https://mangoismycat.github.io/port/' },
+            { id: 7, title: 'Portfolio', author: '김태호', image: 'images/works/김태호.png', link: 'https://kimtaehh.github.io/kkth1234/' },
+            { id: 8, title: 'RYU Summary', author: '류시은', image: 'images/works/류시은.png', link: 'https://ryu-sieun.github.io/2301110153_RYU/' },
+            { id: 9, title: 'I WILL MAKE MY DESIGN PATH', author: '박가람', image: 'images/works/박가람.png', link: 'https://1220asc.github.io/test1113_c/' },
+            { id: 10, title: 'Communicative Designer', author: '심재정', image: 'images/works/심재정.png', link: 'https://simpsonii.github.io/shimjaejeong-Portfolio-Sites/' },
+            { id: 11, title: 'A NEW Encounter with Ohui', author: '양성우', image: 'images/works/양성우.jpg', link: 'https://yangseongwoo.github.io/ohuiwebsite/' },
+            { id: 12, title: '그린피스 웹사이트 리디자인', author: '우수지', image: 'images/works/우수지.jpg', link: 'https://wsj6328.github.io/green/' },
+            { id: 13, title: '그리드와 소금', author: '원세연', image: 'images/works/원세연.png', link: 'https://seyeonwon.github.io/web__portfolio/' },
+            { id: 14, title: 'What’s on my desk', author: '유지수', image: 'images/works/유지수.jpg', link: 'https://wltn510.github.io/1107/' },
+            { id: 15, title: '서울 자전거 따릉이', author: '윤 비', image: 'images/works/윤비.png', link: 'https://drive.google.com/file/d/1lYo49TlrjjIXF-Ev-oR05J9aIZa0Ygw8/view?usp=sharing' },
+            { id: 16, title: '카카오뱅크 웹사이트 리디자인', author: '이세윤', image: 'images/works/이세윤1.jpg', link: 'https://leeeseyoon.github.io/kakaobank_2/' },
+            { id: 17, title: '위홈 앱 리디자인', author: '이세윤', image: 'images/works/이세윤2.jpg', link: 'https://drive.google.com/file/d/13hy-fX0RTd7LBDh5lOAvT34jYPpwB-FR/view?usp=sharing' },
+            { id: 18, title: 'Portfolio', author: '이지우', image: 'images/works/이지우.jpg', link: 'https://jiwooyoooooo.github.io/portfolio/#works' },
+            { id: 19, title: '광염 소나타 책 표지', author: '임 율', image: 'images/works/임율.jpg', link: 'https://drive.google.com/file/d/10WNQMcpWPJCUxRxZ1LAAWGT8Wj2dJjef/view?usp=sharing' },
+            { id: 20, title: '유튜브 앱 리디자인', author: '임현준', image: 'images/works/임현준.png', link: 'https://drive.google.com/file/d/1GzokKcUFusyRB9sxcCd2erSKArgJD_2h/view?usp=sharing' },
+            { id: 21, title: 'Portfolio', author: '장천수', image: 'images/works/장천수.png', link: 'https://1000soo.github.io/cs_work/' },
+            { id: 22, title: 'Portfolio', author: '최은지', image: 'images/works/최은지.jpg', link: 'https://choieunjiji.github.io/port/' },
+            { id: 23, title: '프라임 비디오 앱', author: '최훈석', image: 'images/works/최훈석.png', link: 'https://drive.google.com/file/d/1d0Hjt6WTWM_oZm4IKAXztCFNDly1bO60/view?usp=sharing' },
+            { id: 24, title: '크린나라 앱 리디자인', author: '황세희', image: 'images/works/황세희.png', link: 'https://drive.google.com/file/d/1j2KgnJsLtmfSR2nEsCLlLPtj3R-u2nWt/view?usp=sharing' }
         ],
         visual: [
-            { title: 'ANAKNE(굿즈디자인)', author: '강찬우', image: 'images/works/강찬우.jpg', link: 'https://drive.google.com/file/d/1-5p2puEUfejx0NdwvQJxGycwxog3qRJb/view?usp=sharing' },
-            { title: '제주여행', author: '김승빈', image: 'images/works/김승빈.png', link: 'https://drive.google.com/file/d/1QDHBKdcCQEl1Hz57p1gNmFfiEW_wFBVI/view?usp=sharing' },
-            { title: '하이트 100주년', author: '박경호', image: 'images/works/박경호.jpg', link: 'https://drive.google.com/file/d/1JYEX7Bb5MKqITndgL7u1z4ZebisUwOf2/view?usp=sharing' },
-            { title: 'Portfolio', author: '박윤민', image: 'images/works/박윤민.jpg', link: 'https://drive.google.com/file/d/1GHANHtrOuKIimBnNvmxxaisrj8jZWoqR/view?usp=sharing' },
-            { title: 'Graphic Design', author: '송다은', image: 'images/works/송다은.jpg', link: 'https://drive.google.com/file/d/1TGk503galsrlou9JpzQZFfuclXPf5lGH/view?usp=sharing' },
-            { title: '여우의 하루', author: '송민서', image: 'images/works/송민서.jpg', link: 'https://drive.google.com/file/d/1-WOBK5PiuGfcgHSMhsClkyd0uP1L-w55/view?usp=sharing' },
-            { title: 'portfolio', author: '윤혜원', image: 'images/works/윤혜원.jpg', link: 'https://drive.google.com/file/d/1YFjJnD5ciFTtqYTzta2BL_8t7-Xp0rOF/view?usp=sharing' },
-            { title: 'portfolio', author: '이동현', image: 'images/works/이동현.jpg', link: 'https://drive.google.com/file/d/1CVCFoFOQ5fOGc9yDPeYqnqYB5LsA6tmY/view?usp=sharing' },
-            { title: 'Portfolio', author: '이연지', image: 'images/works/이연지.jpg', link: 'https://drive.google.com/file/d/1SsdhXXvdTcAYCNwNaOJkCKaeZ4ZlWVVW/view?usp=sharing' },
-            { title: '앨범 리디자인', author: '이영은', image: 'images/works/이영은.jpg', link: 'https://drive.google.com/file/d/1TP5jZgFGUddZTncCX4OYllb-6rPLeLFc/view?usp=sharing' },
-            { title: '브랜드 B.I디자인', author: '이재원', image: 'images/works/이재원.jpg', link: 'https://drive.google.com/file/d/1pAGVclxw72Iv2w8pRNQhFIkzO-XoFMy7/view?usp=sharing' },
-            { title: '월인석봉에디션', author: '임재형', image: 'images/works/임재형.jpg', link: 'https://drive.google.com/file/d/1r_Nc5pBIytnpiUyiO30-uzF3cxF7tlr8/view?usp=sharing' },
-            { title: '킹부각 (패키지 디자인)', author: '정회강', image: 'images/works/정회강.jpg', link: 'https://drive.google.com/file/d/1ZtJoyxB2d-WY2_Z2Z-FHbLpusA1pg8ti/view?usp=sharing' },
-            { title: '바라의 하루', author: '최민주', image: 'images/works/최민주.jpg', link: 'https://drive.google.com/file/d/149bOHSebmTfuqB5cDmzQu0a0oVgRa3ER/view?usp=sharing' },
-            { title: '자수롭게(로고, 굿즈디자인)', author: '한병헌', image: 'images/works/한병헌.jpg', link: 'https://drive.google.com/file/d/1Z9zKmaU519uUduZqhRglceRT5Zp2GRty/view?usp=sharing' },
-            { title: 'Portfolio', author: '허준우', image: 'images/works/허준우.jpg', link: 'https://drive.google.com/file/d/1O03MhrdLF11GXe7JPpNPPtUOCfKKBzN6/view?usp=sharing' }
+            { id: 1, title: 'ANAKNE(굿즈디자인)', author: '강찬우', image: 'images/works/강찬우.jpg', link: 'https://drive.google.com/file/d/1-5p2puEUfejx0NdwvQJxGycwxog3qRJb/view?usp=sharing' },
+            { id: 2, title: '제주여행', author: '김승빈', image: 'images/works/김승빈.png', link: 'https://drive.google.com/file/d/1QDHBKdcCQEl1Hz57p1gNmFfiEW_wFBVI/view?usp=sharing' },
+            { id: 3, title: '하이트 100주년', author: '박경호', image: 'images/works/박경호.jpg', link: 'https://drive.google.com/file/d/1JYEX7Bb5MKqITndgL7u1z4ZebisUwOf2/view?usp=sharing' },
+            { id: 4, title: 'Portfolio', author: '박윤민', image: 'images/works/박윤민.jpg', link: 'https://drive.google.com/file/d/1GHANHtrOuKIimBnNvmxxaisrj8jZWoqR/view?usp=sharing' },
+            { id: 5, title: 'Graphic Design', author: '송다은', image: 'images/works/송다은.jpg', link: 'https://drive.google.com/file/d/1TGk503galsrlou9JpzQZFfuclXPf5lGH/view?usp=sharing' },
+            { id: 6, title: '여우의 하루', author: '송민서', image: 'images/works/송민서.jpg', link: 'https://drive.google.com/file/d/1-WOBK5PiuGfcgHSMhsClkyd0uP1L-w55/view?usp=sharing' },
+            { id: 7, title: 'portfolio', author: '윤혜원', image: 'images/works/윤혜원.jpg', link: 'https://drive.google.com/file/d/1YFjJnD5ciFTtqYTzta2BL_8t7-Xp0rOF/view?usp=sharing' },
+            { id: 8, title: 'portfolio', author: '이동현', image: 'images/works/이동현.jpg', link: 'https://drive.google.com/file/d/1CVCFoFOQ5fOGc9yDPeYqnqYB5LsA6tmY/view?usp=sharing' },
+            { id: 9, title: 'Portfolio', author: '이연지', image: 'images/works/이연지.jpg', link: 'https://drive.google.com/file/d/1SsdhXXvdTcAYCNwNaOJkCKaeZ4ZlWVVW/view?usp=sharing' },
+            { id: 10, title: '앨범 리디자인', author: '이영은', image: 'images/works/이영은.jpg', link: 'https://drive.google.com/file/d/1TP5jZgFGUddZTncCX4OYllb-6rPLeLFc/view?usp=sharing' },
+            { id: 11, title: '브랜드 B.I디자인', author: '이재원', image: 'images/works/이재원.jpg', link: 'https://drive.google.com/file/d/1pAGVclxw72Iv2w8pRNQhFIkzO-XoFMy7/view?usp=sharing' },
+            { id: 12, title: '월인석봉에디션', author: '임재형', image: 'images/works/임재형.jpg', link: 'https://drive.google.com/file/d/1r_Nc5pBIytnpiUyiO30-uzF3cxF7tlr8/view?usp=sharing' },
+            { id: 13, title: '킹부각 (패키지 디자인)', author: '정회강', image: 'images/works/정회강.jpg', link: 'https://drive.google.com/file/d/1ZtJoyxB2d-WY2_Z2Z-FHbLpusA1pg8ti/view?usp=sharing' },
+            { id: 14, title: '바라의 하루', author: '최민주', image: 'images/works/최민주.jpg', link: 'https://drive.google.com/file/d/149bOHSebmTfuqB5cDmzQu0a0oVgRa3ER/view?usp=sharing' },
+            { id: 15, title: '자수롭게(로고, 굿즈디자인)', author: '한병헌', image: 'images/works/한병헌.jpg', link: 'https://drive.google.com/file/d/1Z9zKmaU519uUduZqhRglceRT5Zp2GRty/view?usp=sharing' },
+            { id: 16, title: 'Portfolio', author: '허준우', image: 'images/works/허준우.jpg', link: 'https://drive.google.com/file/d/1O03MhrdLF11GXe7JPpNPPtUOCfKKBzN6/view?usp=sharing' }
         ]
     };
 
@@ -258,6 +247,7 @@ function initializeMobile() {
         projects.forEach((project, index) => {
             const card = document.createElement('div');
             card.className = 'project-card';
+            card.id = `project-${index}`; // 각 프로젝트에 고유 ID 추가
             card.innerHTML = `
                 <a href="${project.link}" target="_blank" class="project-link">
                     <div class="project-image" style="background-image: url('${project.image}')"></div>
@@ -459,48 +449,5 @@ const listViewBtn = document.querySelector('.list-view');
 
 gridViewBtn.addEventListener('click', () => setActiveView('grid'));
 listViewBtn.addEventListener('click', () => setActiveView('list'));
-
-document.addEventListener('DOMContentLoaded', () => {
-    // 세션 스토리지에서 이전 방문 여부 확인
-    const hasVisited = sessionStorage.getItem('hasVisitedWorks');
-    
-    // 카테고리 텍스트 요소들
-    const categoryTexts = document.querySelectorAll('.category-text');
-    
-    if (!hasVisited) {
-        // 첫 방문 시에만 Digital을 기본값으로 설정
-        categoryTexts.forEach(text => {
-            if (text.textContent.toLowerCase() === 'digital') {
-                text.classList.add('active');
-            } else {
-                text.classList.remove('active');
-            }
-        });
-        
-        // 방문 표시 저장
-        sessionStorage.setItem('hasVisitedWorks', 'true');
-    }
-
-    // 카테고리 전환 이벤트
-    categoryTexts.forEach(text => {
-        text.addEventListener('click', () => {
-            const category = text.textContent.toLowerCase();
-            
-            // 카테고리 텍스트 활성화
-            categoryTexts.forEach(t => t.classList.remove('active'));
-            text.classList.add('active');
-            
-            // 프로젝트 리스트 전환
-            document.querySelectorAll('.project-list').forEach(list => {
-                list.classList.remove('active');
-                if (list.classList.contains(category)) {
-                    list.classList.add('active');
-                }
-            });
-        });
-    });
-
-    // ... 나머지 코드는 그대로 유지
-});
 
 
